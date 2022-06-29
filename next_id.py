02a7506eec6e5d9b37dc6522256478a32b30264a
mj <mj@wineta.wineta.dom>
2022-06-29 16:54:12 +0200

    Formating.

--- next_id.py
+++ next_id.py
@@ -1,31 +1,31 @@
 id_values = '0123456789abcdefghijklmnoprtuwvsxyz'
 
+
 def next_position(position):
-  if position=='z':
-    position='0'
-    return position, False
-  position= id_values[id_values.index(position)+1]
-  return position, True
+    if position == 'z':
+        position = '0'
+        return position, False
+    position = id_values[id_values.index(position) + 1]
+    return position, True
+
 
 def next_id(param_string):
-  param = list(param_string)
-  for position in range(1,len(param)):
-    old_value = param[len(param)-position]
-    param[len(param)-position], result = next_position(old_value)
-    if result:
-      break
-    if position==len(param)-1:
-      return '0'*len(param)
-  return "".join(param)
+    param = list(param_string)
+    for position in range(1, len(param)):
+        old_value = param[len(param) - position]
+        param[len(param) - position], result = next_position(old_value)
+        if result:
+            break
+        if position == len(param) - 1:
+            return '0' * len(param)
+    return "".join(param)
+
 
 param = '0zzz111'
-if '0zzz112' == next_id(param) :
-  print('OK 1')
+assert '0zzz112' == next_id(param)
 
 param = 'zzzzz'
-if '00000' == next_id(param) :
-  print('OK 2')
+assert '00000' == next_id(param)
 
 param = '0000z'
-if '00010' == next_id(param) :
-  print('OK 3')
+assert '00010' == next_id(param)
